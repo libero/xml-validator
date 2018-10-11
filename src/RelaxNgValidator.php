51c9f74035e7dc390fac2801ebb00629b08b6f0f
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-10-11 16:07:35 +0100

    Extract LibXmlValidator

--- src/RelaxNgValidator.php
+++ src/RelaxNgValidator.php
@@ -5,14 +5,11 @@ declare(strict_types=1);
 namespace Libero\XmlValidator;
 
 use DOMDocument;
-use function array_map;
-use function count;
-use function libxml_clear_errors;
-use function libxml_get_errors;
-use function libxml_use_internal_errors;
 
 final class RelaxNgValidator implements XmlValidator
 {
+    use LibXmlValidator;
+
     private $schema;
 
     public function __construct(string $schema)
@@ -20,20 +17,8 @@ final class RelaxNgValidator implements XmlValidator
         $this->schema = $schema;
     }
 
-    public function validate(DOMDocument $document) : void
+    protected function doValidation(DOMDocument $document) : void
     {
-        $internalErrors = libxml_use_internal_errors(true);
-        libxml_clear_errors();
-
         @$document->relaxNGValidate($this->schema);
-
-        $errors = array_map([Failure::class, 'fromLibXmlError'], libxml_get_errors());
-
-        libxml_clear_errors();
-        libxml_use_internal_errors($internalErrors);
-
-        if (count($errors) > 0) {
-            throw new ValidationFailed($errors);
-        }
     }
 }
